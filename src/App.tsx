3f156508376d66d9826dd9b837445fb2772e2ddf
Adam Rackis <adamrackis@hotmail.com>
2025-05-08 08:36:04 -0500

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
+import { StoreDemo1 } from "./StoreDemo1";
 
 function App() {
   return (
     <div className="m-5 p-5 flex flex-col gap-2">
-      <div>A</div>
-      <div>A</div>
-      <div>A</div>
+      <StoreDemo1 />
+      <StoreDemo1 />
     </div>
   );
 }
