b6df80068c04300ef8cac6f04bf8203effa4bbc8
Adam Rackis <adamrackis@hotmail.com>
2025-05-28 23:02:07 -0500

    work

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import "./App.css";
 
 import { useTasksStore } from "./tasks-store";
 import { tasks } from "./dummy-tasks-data";
+import { TasksView } from "./components/tasks-view";
 
 function App() {
-  const { setTasks, setTasksView, setCurrentUserFilter } = useTasksStore();
+  const { setTasks, setCurrentView, setCurrentUserFilter } = useTasksStore();
 
   useEffect(() => {
     setTasks(tasks);
-    setTasksView("list");
+    setCurrentView("list");
     setCurrentUserFilter("Adam");
   }, []);
 
-  return <div className="m-5 p-5 flex flex-col gap-2"></div>;
+  return (
+    <div className="m-5 p-5 flex flex-col gap-2">
+      <TasksView />
+    </div>
+  );
 }
 
 export default App;
