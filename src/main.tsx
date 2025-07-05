d4e98d8b5745269d55d665a0ab44012454a01e3d
Adam Rackis <adamrackis@hotmail.com>
2025-07-05 17:52:45 -0500

    Add new zustand version - style tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,31 @@ import ZustandApp from "./zustand/ZustandApp";
 import VanillaApp from "./vanilla/VanillaApp";
 
 import "./App.css";
+import { cn } from "./utils";
 
 const Root: FC = () => {
-  const [app, setApp] = useState<"zustand" | "vanilla">("zustand");
+  const [app, setApp] = useState<"zustand" | "vanilla" | "zustand-optimized">("zustand");
 
   return (
     <div className="p-2">
       <div className="flex gap-2">
-        <button className="border p-2 rounded text-gray-700" onClick={() => setApp("zustand")}>
+        <button
+          className={cn("border p-2 rounded text-gray-700", app === "vanilla" && "bg-blue-500 text-white")}
+          onClick={() => setApp("vanilla")}
+        >
+          Vanilla
+        </button>
+        <button
+          className={cn("border p-2 rounded text-gray-700", app === "zustand" && "bg-blue-500 text-white")}
+          onClick={() => setApp("zustand")}
+        >
           Zustand
         </button>
-        <button className="border p-2 rounded text-gray-700" onClick={() => setApp("vanilla")}>
-          Vanilla
+        <button
+          className={cn("border p-2 rounded text-gray-700", app === "zustand-optimized" && "bg-blue-500 text-white")}
+          onClick={() => setApp("zustand-optimized")}
+        >
+          Zustand Optimized
         </button>
       </div>
       {app === "zustand" ? <ZustandApp /> : <VanillaApp />}
