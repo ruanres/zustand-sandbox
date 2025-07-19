f7f6193d1ca801874743e604fb0497f5ae7e6036
Adam Rackis <adamrackis@hotmail.com>
2025-07-19 16:06:59 -0500

    logging tweaks

--- src/vanilla/VanillaApp.tsx
+++ src/vanilla/VanillaApp.tsx
@@ -6,7 +6,7 @@ import { TasksCount } from "./components/tasks-count";
 import { Filter } from "./components/tasks-filter";
 
 function App() {
-  console.log("Rendering AppContent");
+  console.log("Rendering App");
 
   return (
     <div className="m-5 p-5 flex flex-col gap-2">
